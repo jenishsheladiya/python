3b08d04c1b21983c98b2eea7891e1731177d9090
jenish_sheladiya <sheladiyajenish44@gmail.com>
2025-05-09 11:15:51 +0530

    abstraction6.py
    Shape, Circle, Square and define area using abstract method

---
+++ abstraction6.py
@@ -0,0 +1,27 @@
+# Shape, Circle, Square and define area using abstractmethod
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+
+circle = Circle(5)
+square = Square(4)
+print("Circle area:", circle.area())
+print("Square area:", square.area())
