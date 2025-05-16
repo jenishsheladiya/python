ff9f8d00ae21938398547b836228e3899960f52a
jenish_sheladiya <sheladiyajenish44@gmail.com>
2025-05-16 10:14:32 +0530

    encapsulation2.py
    Bank Account Security

---
+++ encapsulation2.py
@@ -0,0 +1,14 @@
+#  Bank Account Security
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+acc = BankAccount(1000)
+acc.deposit(500)
+print(acc.get_balance())  # Output: 1500
