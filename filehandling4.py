c22ba5ab10f287b5e48e74651968d34641f12720
jenish_sheladiya <sheladiyajenish44@gmail.com>
2025-05-18 11:45:57 +0530

    filehandling4.py
    create a file and add the text on file

---
+++ filehandling4.py
@@ -0,0 +1,6 @@
+
+# create a file and add the text on file
+with open("sample.txt", "w") as f:
+    f.write("Hi everyone\nwe are learning File I/O\n")
+    f.write("usnig Java.\ni like programming in Java.")
+    
