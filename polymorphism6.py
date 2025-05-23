8dd1289534b2e7400d5b24c6b08d6af3be57f51c
jenish_sheladiya <sheladiyajenish44@gmail.com>
2025-05-23 19:25:40 +0530

    polymorphism6.py
    Polymorphism with Class Methods

---
+++ polymorphism6.py
@@ -0,0 +1,14 @@
+# Polymorphism with Class Methods
+class Dog:
+    def sound(self):
+        print("Bark")
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+def make_sound(animal):
+    animal.sound()
+
+make_sound(Dog())
+make_sound(Cat())
